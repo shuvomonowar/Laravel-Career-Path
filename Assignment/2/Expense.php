9282acda6aa5ed14e88da5b8c790a0330b82d29e
shuvomonowar <monowarhossain41998@gmail.com>
2023-09-28 20:35:05 +0600

    Assignment-2 completed successfully

---
+++ Assignment/2/Expense.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+class Expense
+{
+    private string $name;
+    private Categories $categories;
+    private int $amount;
+
+    function getName(): string
+    {
+        return $this->name;
+    }
+
+    function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    function getCategories(): Categories
+    {
+        return $this->categories;
+    }
+
+    function setCategories(Categories $categories): void
+    {
+        $this->categories = $categories;
+    }
+
+    function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    function setAmount(int $amount): void
+    {
+        $this->amount = $amount;
+    }
+}
