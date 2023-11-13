0e8b22d027bdb88ede0dcf64db2377686b3c8a99
Monowar Hossain Shuvo <67220088+shuvomonowar@users.noreply.github.com>
2023-11-13 13:46:54 +0600

    Delete Assignment/2 directory

--- Assignment/2/ActionToDatabase.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface ActionToDatabase
-{
-    public function storeToDatabase(array $dataArray): void;
-    public function loadFromDatabase(): array;
-}
--- Assignment/2/Categories.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-enum Categories: string {
-    case INCOME = "Income";
-    case EXPENSE = "Expense";
-}
